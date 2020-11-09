9a08d84b906731468396cde1606ad353850ab5ba
King Bobby III <65513795+KingBobbyIII@users.noreply.github.com>
2020-11-10 12:22:38 +1300

    Add files via upload

--- Python#5(Boring).py
+++ Python#5(Boring).py
@@ -1,3 +1,5 @@
-def my_function ():
-    print("Hello from a function")
-    my_function()
+def calc_area ():
+    Length = input("ples enetr the length of as redctangle")
+    width = input)'plesa netera lenght of a rectangle'(
+        eraa = Length * width
+        print)eraa(
